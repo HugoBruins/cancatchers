ba87d0669f80c9958b82372f7bf0142986e00788
Hugo Bruins <hugobruins@gmail.com>
2021-03-05 19:51:36 +0100

    Update HyperOmegaSuperCode.ino

--- arduino/HyperOmegaSuperCode.ino
+++ arduino/HyperOmegaSuperCode.ino
@@ -1,66 +1,55 @@
-/***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
-  Designed specifically to work with the Adafruit BMEP280 Breakout
-  ----> http://www.adafruit.com/products/2651
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
 #include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(6,5);
-
-SoftwareSerial ss(3, 2);
-
+#include <TinyGPS++.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <SD.h>
-const int chipSelect = 4;
 
+SoftwareSerial mySerial(6, 5);
+TinyGPSPlus gps;
+
+static const int RXPin = 3, TXPin = 2;
+static const uint32_t GPSBaud = 9600;
+SoftwareSerial ss(RXPin, TXPin);
+
+const int chipSelect = 4;
 Adafruit_BMP280 bmp; // I2C
 const int BMP_address = 0x76;
 String filename = "data";
 
-//aerospace engineering variabelen :)
+String gpsLat = "-";
+String gpsLon = "-";
 
 //dit is nodig om later het verschil in luchtdruk te meten
-float gemiddeldeLuchtdruk;     
+float gemiddeldeLuchtdruk;
 float gemiddeldeTemperatuur;
 
 float constante;
 float lapseRate = -0.0065;     //kelvin per meter in de troposfeer
 int R = 287;                   //gasconstante
 float g = -9.81;               //zwaartekracht volgens SIA
-
 float hoogte;
 
 
 
 void setup() {
-  randomSeed(analogRead(1));
   Serial.begin(9600);
-  ss.begin(9600);
-  
-  
-  
-  
   mySerial.begin(9600);
+  ss.begin(GPSBaud);
+  randomSeed(analogRead(1));
+
+
   Serial.println("Initializing SD card...");
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
     mySerial.println("sdKaart faalt hem jammer dit");
   }
-  
+
   bmp.begin(BMP_address);
 
   constante = (1 / (g / (lapseRate * R)));
-  
+
   //de sensor warmdraaien voor de nulmeting
   for (int i = 0; i <= 100; i++) {
     bmp.readPressure();
@@ -72,18 +61,18 @@ void setup() {
     gemiddeldeLuchtdruk += bmp.readPressure();
     gemiddeldeTemperatuur += bmp.readTemperature();
   }
-  
+
   gemiddeldeLuchtdruk = gemiddeldeLuchtdruk / 1000;
-  gemiddeldeTemperatuur = (gemiddeldeTemperatuur / 1001)+ 273.15;
-  
+  gemiddeldeTemperatuur = (gemiddeldeTemperatuur / 1000) + 273.15;
+
 
   //geeft het bestand een willekeurig nummer aan het einde, zodat elke datalezing uniek is zonder dat er internet nodig is (voor tijd).
   int randomnumber =  random(10000);
   String nummer = String(randomnumber);
   filename = filename + nummer + ".txt";
-  
+
   //debug
-  
+
   Serial.println(gemiddeldeLuchtdruk);
   Serial.println(gemiddeldeTemperatuur);
   mySerial.println(gemiddeldeLuchtdruk);
@@ -101,32 +90,44 @@ void setup() {
 
 
 void loop() {
-  byte gpsData = ss.read();
-  Serial.write(gpsData);
+  while (ss.available() > 0)
+    if (gps.encode(ss.read())) {
+      if (gps.location.isValid())
+      {
+        gpsLat = String((gps.location.lat(), 6));
+        gpsLon = String((gps.location.lng(), 6));
+      }
+      else
+      {
+        gpsLat = "0";
+        gpsLon = "0";
+      }
+    }
 
+  if (millis() > 10000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
 
-  
   String luchtdruk = String(round(bmp.readPressure()));
   String temperatuur = String(bmp.readTemperature());
   float luchtdruk2 = luchtdruk.toFloat();
 
   //hoogte berekenen
   float drukverschil = luchtdruk2 / gemiddeldeLuchtdruk;
-  float temperatuurOpHoogte = gemiddeldeTemperatuur * (pow(drukverschil,constante));
+  float temperatuurOpHoogte = gemiddeldeTemperatuur * (pow(drukverschil, constante));
   String hoogte = String((temperatuurOpHoogte - gemiddeldeTemperatuur) / lapseRate);
-  
-  String dataString = hoogte + " " + temperatuur + " " + luchtdruk;
- 
+
+  String dataString = gpsLat + " " + gpsLon + " " + hoogte + " " + temperatuur + " " + luchtdruk;
+
   File dataFile = SD.open(filename, FILE_WRITE);
   if (dataFile) {
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
   }
-  else {
-    //Serial.println("error opening datalog.txt");
-  }
-  
+
   Serial.println(dataString);
   mySerial.println(dataString);
   delay(1000);
