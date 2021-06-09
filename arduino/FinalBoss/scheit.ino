9be4a88169df3c6730ea61f0d83772820d678ce7
Hugo Bruins <hugobruins@gmail.com>
2021-06-09 10:46:49 +0200

    Update and rename scheit to scheit.ino
    cleaned up scheit

---
+++ arduino/FinalBoss/scheit.ino
@@ -0,0 +1,44 @@
+#include <NMEAGPS.h>
+#include <NeoSWSerial.h>
+#include <EBYTE.h>
+#include <GPSport.h>
+
+#define PIN_RX 3
+#define PIN_TX 5
+#define PIN_M0 7
+#define PIN_M1 6
+#define PIN_AX 2
+
+NMEAGPS  gps;
+gps_fix  fix;
+NeoSWSerial ESerial(PIN_RX, PIN_TX);
+EBYTE Transmitter(&ESerial, PIN_M0, PIN_M1, PIN_AX);
+
+struct DATA {
+  float longitude = 0; 
+  float latitude = 0; 
+};
+DATA CanSatData;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  ESerial.begin(9600);
+  gpsPort.begin(9600);
+}
+
+//--------------------------
+
+void loop()
+{
+  while (gps.available( gpsPort )) {
+    fix = gps.read();
+    
+    CanSatData.longitude++; 
+    while(!Serial);
+    Transmitter.SendStruct(&CanSatData, sizeof(CanSatData));
+    Serial.print("Sending: "); Serial.println(CanSatData.longitude);
+  }
+}
+
