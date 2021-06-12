b4d2c5f6651e3b33648f335973efc696d7fabb38
Hugo Bruins <hugobruins@gmail.com>
2021-06-12 09:42:50 +0200

    Update cansat_code_all.ino

--- arduino/cansat_code/cansat_code_all.ino
+++ arduino/cansat_code/cansat_code_all.ino
@@ -1,4 +1,4 @@
-//this cansat code does everything except for writing to the SD card
+//the cansat code that features EVERYTHING!
 /*
    I will let Thomas handle all the pins here :)
    
@@ -16,20 +16,18 @@
 #include <LoRa_E32.h>
 #include <NMEAGPS.h>
 #include <GPSport.h>
-#include <BMP280_DEV.h>  
-
-
+#include <Adafruit_BMP280.h>
+#include <SdFat.h>
+#include <SPI.h>
 
 LoRa_E32 e32ttl(3, 5, 2, 7, 6);
 NMEAGPS  gps;
 gps_fix  fix;
-BMP280_DEV bmp; // I2C
+Adafruit_BMP280 bmp; // I2C
+
 //all the altitude calculation variables
 float AVERAGE_PRESSURE;
 float AVERAGE_TEMPERATURE;
-float temperature;
-float pressure;
-
 
 //the struct that gets send
 struct CansatData {
@@ -38,62 +36,116 @@ struct CansatData {
   float longitude;
   float altitude;
   float temperature;
-  float pressure;
+  int pressure;
 };
+
+//sd card using sdFAT
+SdFat SD;
+const int chipSelect = 4;
+File logfile;
 
 void setup()
 {
   DEBUG_PORT.begin(9600);
-  gpsPort.begin(9600); //we may or may not have overclocked our gps module a bit
+  gpsPort.begin(115200); //we may or may not have overclocked our gps module a bit
   e32ttl.begin();
-  while (!Serial);
+  while (!DEBUG_PORT);
   delay(100);
-  
+
+  //sd card
+  initSD();
+
   //bmp module
   if (!bmp.begin(0x76)) {
     DEBUG_PORT.println(F("BMP280 failed"));
   }
 
-  bmp.startNormalConversion();
-  
   //warming up the sensor a bit, otherwise battery power seems to interfere.
   for (int i = 0; i <= 50; i++) {
-    bmp.getCurrentTempPres(temperature, pressure);
+    bmp.readPressure();
+    bmp.readTemperature();
     delay(1);
   }
   //getting starting pressure and temperature based on a lot of measurements
   for (int i = 0; i <= 499; i++) {
-    bmp.getCurrentTempPres(temperature, pressure);
-    AVERAGE_PRESSURE += temperature;
-    AVERAGE_TEMPERATURE += pressure;
+    AVERAGE_PRESSURE += bmp.readPressure();
+    AVERAGE_TEMPERATURE += bmp.readTemperature();
     delay(1);
   }
   AVERAGE_PRESSURE /= 500;
   AVERAGE_TEMPERATURE = (AVERAGE_TEMPERATURE / 500) + 273.15;
-  DEBUG_PORT.println(AVERAGE_PRESSURE);
-  DEBUG_PORT.println(AVERAGE_TEMPERATURE);
 }
 
 void loop()
 {
   while (gps.available( gpsPort )) {
     fix = gps.read();
-    bmp.getCurrentTempPres(temperature, pressure);
+
+    static uint16_t lastLoggingTime  = 0;
+    uint16_t startLoggingTime = millis();
+
     struct CansatData {
       float latitude = fix.latitude();
       float longitude = fix.longitude();
       int sats = fix.satellites;
-      float temperature = temperature;
-      float pressure = pressure;
-  
+
+      float temperature = bmp.readTemperature();
+      float pressure = bmp.readPressure();
+
       //-0.0065 is the lapse rate in the troposphere, 0.190163099 is a constant, defined by ((a*R)/g)
       //-153.8461538 is the inverse of the lapse rate (-0.0065), I have a feeling that float multiplication is slightly faster than division
       //the altitude is based on terrain altitude, so there has to be no software adjusting during launch
-      float altitude = ( ( AVERAGE_TEMPERATURE * (pow( (pressure / AVERAGE_PRESSURE) , 0.190163099)) ) - (AVERAGE_TEMPERATURE) ) * -153.8461538;
+      int altitude = ( ( AVERAGE_TEMPERATURE * (pow( (bmp.readPressure() / AVERAGE_PRESSURE) , 0.190163099)) ) - (AVERAGE_TEMPERATURE) ) * -153.8461538;
 
     } CansatData;
 
+    logfile.print(CansatData.sats); logfile.print(F(","));
+    logfile.print(CansatData.latitude,6); logfile.print(F(","));
+    logfile.print(CansatData.longitude,6); logfile.print(F(","));
+    logfile.print(CansatData.altitude); logfile.print(F(","));
+    logfile.print(CansatData.temperature); logfile.print(F(","));
+    logfile.print(CansatData.pressure); logfile.print(F(","));
+    logfile.println(millis());
+
+    static uint16_t lastFlushTime = 0;
+    if (startLoggingTime - lastFlushTime > 1000) {
+        lastFlushTime = startLoggingTime;
+        logfile.flush();
+    }
+    lastLoggingTime = (uint16_t) millis() - startLoggingTime;
+
     ResponseStatus rs = e32ttl.sendFixedMessage(0, 3, 4, &CansatData, sizeof(CansatData));
+
+
+
     DEBUG_PORT.println(F(":)"));
   }
+}
+
+void initSD()
+{
+  DEBUG_PORT.println( F("Initializing SD card...") );
+  if (!SD.begin(chipSelect)) {
+    DEBUG_PORT.println( F("  SD card failed, or not present") );
+  }
+  DEBUG_PORT.println( F("  SD card initialized.") );
+
+  // Pick a numbered filename, 00 to 99.
+  char filename[15] = "data_##.txt";
+  for (uint8_t i=0; i<100; i++) {
+    filename[5] = '0' + i/10;
+    filename[6] = '0' + i%10;
+    if (!SD.exists(filename)) {
+      break;
+      }
+    }
+
+  logfile = SD.open(filename, FILE_WRITE);
+  if (!logfile) {
+    DEBUG_PORT.print( F("Couldn't create ") ); 
+      DEBUG_PORT.println(filename);
+  }
+
+  DEBUG_PORT.print( F("Writing to ") ); 
+  DEBUG_PORT.println(filename);
 }
